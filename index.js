5266f338839359bfe411152baeac5dd91a28a3a6
Jesus Vega <chuyspc@gmail.com>
2017-01-23 10:53:25 +0000

    Done.

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-
+  object[key] = value;
+  return object;
 }
 
 function deleteFromObjectByKey(object, key) {
-
+  var cloned_Obj = Object.assign({}, object);
+  delete cloned_Obj[key];
+  return cloned_Obj;
 }
 
-function deleteFromObjectByKey(object, key) {
-  
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
 }
