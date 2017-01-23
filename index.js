2e1196ceabc58c4d287efd2d87929edee025962d
Jesus Vega <chuyspc@gmail.com>
2017-01-23 10:45:54 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  var cloned_Obj = Object.assign({} , object, { [key]: value});
+  return cloned_Obj;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+
+}
+
+function deleteFromObjectByKey(object, key) {
+
+}
+
+function deleteFromObjectByKey(object, key) {
+  
+}
